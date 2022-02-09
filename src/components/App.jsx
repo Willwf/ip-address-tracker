99fb41c9b53d7bb275d95aad8d3a1899d6cda9f9
William Firmino <williamwf07@gmail.com>
2022-02-09 14:05:49 -0300

    Batman! (this commit has no parents)

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { AppDiv, Title } from "./styles.js";
+
+function App() {
+  return (
+    <AppDiv>
+      <Title>Hello World!</Title>
+    </AppDiv>
+  );
+}
+
+export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppDiv = styled.div`
+  margin: auto;
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+`;
