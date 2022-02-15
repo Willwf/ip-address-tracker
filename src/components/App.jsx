9cfd002c09092cb277951916fb7ff8e7dbc9591b
William Firmino <williamwf07@gmail.com>
2022-02-15 04:09:48 -0300

    Created HTML of the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,44 @@
-import { AppDiv, Title } from "./styles.js";
+import {
+  AppDiv,
+  Button,
+  Info,
+  InfoTitle,
+  InfoValue,
+  Input,
+  InputField,
+  LocationInfo,
+  MapDiv,
+  Title,
+} from "./styles.js";
 
 function App() {
   return (
     <AppDiv>
-      <Title>Hello World!</Title>
+      <Title>IP Address Tracker</Title>
+      <InputField>
+        <Input placeholder="Search for any IP address or domain"></Input>
+        <Button>&gt;</Button>
+      </InputField>
+      <MapDiv>
+        <LocationInfo>
+          <Info>
+            <InfoTitle>Ip Adress</InfoTitle>
+            <InfoValue>192.212.174.101</InfoValue>
+          </Info>
+          <Info>
+            <InfoTitle>Location</InfoTitle>
+            <InfoValue>Brooklyn, NY 10001</InfoValue>
+          </Info>
+          <Info>
+            <InfoTitle>Timezone</InfoTitle>
+            <InfoValue>UTC -05:00</InfoValue>
+          </Info>
+          <Info>
+            <InfoTitle>ISP</InfoTitle>
+            <InfoValue>SpaceX Starlink</InfoValue>
+          </Info>
+        </LocationInfo>
+      </MapDiv>
     </AppDiv>
   );
 }
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,9 +1,29 @@
 import styled from "styled-components";
 
-export const AppDiv = styled.div`
+export const AppDiv = styled.main`
   margin: auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Title = styled.h1`
   font-weight: bold;
 `;
+
+export const InputField = styled.div``;
+export const Input = styled.input``;
+export const Button = styled.button``;
+export const MapDiv = styled.div``;
+
+export const LocationInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Info = styled.div``;
+export const InfoTitle = styled.h2``;
+export const InfoValue = styled.p``;
